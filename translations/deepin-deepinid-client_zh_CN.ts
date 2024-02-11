a8314918fae14cdf26ef467d1bd0a0806a45d01c
gfdgd-xi <3025613752@qq.com>
2024-02-11 15:00:47 +0000

    7.0.2

---
+++ translations/deepin-deepinid-client_zh_CN.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>QObject</name>
+    <message>
+        <source>View</source>
+        <translation>查看</translation>
+    </message>
+    <message>
+        <source>Network Account Privacy Policy</source>
+        <translation>网络帐户隐私政策</translation>
+    </message>
+    <message>
+        <source>Agree and Turn On Network Account Sync</source>
+        <translation>同意并开启网络帐户同步功能</translation>
+    </message>
+</context>
+</TS>
